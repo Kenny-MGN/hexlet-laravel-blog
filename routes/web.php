4078eaa14d6cfd9468065d89e1a07f3d41836e03
Kenny-MGN <kennymgn@gmail.com>
2023-05-07 17:00:58 +0500

    Add PageController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', fn() => view('about'));
+Route::get('about', [PageController::class, 'about']);
