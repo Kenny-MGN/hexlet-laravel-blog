9bb323e32c8ed03d1261b411d4f212d8151039fd
Kenny-MGN <kennymgn@gmail.com>
2023-05-08 12:49:27 +0500

    Add ArticleController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
+        <nav class="menu">
+            <ul>
+                <li><a href="{{ route('article.index') }}">Статьи</a></li>
+            </ul>
+        </nav>
         <div class="container mt-4">
             <h1>@yield('header')</h1>
             <div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', [PageController::class, 'about']);
+Route::get('about', [PageController::class, 'about'])->name('about');
+
+Route::get('articles', [ArticleController::class, 'index'])->name('article.index');
